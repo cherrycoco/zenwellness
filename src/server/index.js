ea1c7a159ef9d0e1535a33f646000dd791717d47
Cherry Xu <cherryxuqianyun@gmail.com>
2018-03-18 14:14:22 -0700

    basic koa server

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,8 @@
 const Koa = require('koa');
+const serve = require('koa-static');
 const app = new Koa();
+app.use(serve('.'));
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
+app.use(serve('../../public/index.html'));
 
 app.listen(3000);
